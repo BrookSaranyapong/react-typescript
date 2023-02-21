80e92884d4a0225864f606ee0762cb2a08949194
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-02-22 03:54:29 +0700

    day 1 complete

--- react-typescript/src/components/AppFooter.tsx
+++ react-typescript/src/components/AppFooter.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
-
+type User = {
+    fullname: string
+}
 const AppFooter = () => {
     // let company = 'TypeScript';
-    const [company, setCompany] = useState()
+    const [company, setCompany] = useState("TypeScript");
+    // const [user, setUser] = useState<User>({ fullname: 'John Doe' });
+    const [user, setUser] = useState<User | null>(null);
     const isShow = false;
 
     const showMsg = () => {
         // alert('Hello TypeScript');
         setCompany("React");
+        setUser({ fullname: 'John Doe' })
 
     }
 
     return (
         <>
             <hr />
+            {user && <p>User: {user?.fullname}</p>}
             <button onClick={showMsg}>Click Me!</button>
             <p>created by {company}</p>
             <p>{new Date().getFullYear()}</p>
