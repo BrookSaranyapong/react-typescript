f44e5230e48db98690643e04826ed2521ad7833b
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-02-25 01:25:26 +0700

    "add Chakra UI Clear CSS use CSS UI framework template"

--- react-typescript/src/main.tsx
+++ react-typescript/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+
 import './index.css'
+import HomePage from './pages/home-page'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <ChakraProvider>
+      <HomePage />
+    </ChakraProvider>
+  </React.StrictMode>
 )
